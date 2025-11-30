988ef3426376d1c47726f4ebbf0b6fe1b0ec38fc
Akankshasinn <25LBCS3153@culkomail.in>
2025-11-30 22:17:26 +0530

    main function

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
 // start writing from here
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_MEDIA 100
+
+// ---- Data structure ----
+typedef struct {
+    int id;
+    char title[100];
+    char artist[100];
+    char genre[50];
+    int year;
+} Media;
+
+Media library[MAX_MEDIA];
+int count = 0;
+
+// ---- Small helper: remove '\n' from fgets ----
+void removeNewline(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
+        i++;
+    }
+}
